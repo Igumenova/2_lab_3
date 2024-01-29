b1b7ed098453436fb5188664eb50219ee0018e4d
Igumenova <128398714+Igumenova@users.noreply.github.com>
2024-01-29 17:23:26 +0300

    2_lab_3

---
+++ task.py
@@ -0,0 +1,46 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    def __str__(self):
+        return f"Книга {self._name}. Автор {self._author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        if not isinstance(pages, int):
+            raise TypeError("Страницы int")
+        if pages <= 0:
+            raise ValueError("Страницы >0")
+        self.pages = pages
+
+    def __str__(self):
+        return f"Книга {self._name}. Автор {self._author}. Количество страниц {self.pages}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self.pages!r})"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        if not isinstance(duration, float):
+            raise TypeError("Продолжительность float")
+        if duration <= 0:
+            raise ValueError("Продолжительность >0")
+        self.duration = duration
+
+
+    def __str__(self):
+        return f"Книга {self._name}. Автор {self._author}. Продолжительность {self.duration}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, duration={self.duration!r})"
+
+
